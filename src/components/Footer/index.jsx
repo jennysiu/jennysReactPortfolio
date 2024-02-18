d71f9e9dbd85d87007ecca4f1e4bd6ddb454ec9e
jennysiu <jenny.siu79@gmail.com>
2024-02-18 01:13:55 +0000

    added links

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,23 @@
+import '@fortawesome/fontawesome-free/css/all.css';
+
+function Footer() {
+  return (
+    <footer>
+      <section className="socialBadges">
+        <a className="" href="https://github.com/jennysiu">
+          <i class="fa-brands fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/jenny-siu-534576156/">
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+        <a href="mailto:jenny.siu79@gmail.com">
+          <i class="fa-solid fa-envelope"></i>
+        </a>
+      </section>  
+      <p>© Jenny Siu. All rights</p>
+    </footer>
+  )
+
+}
+
+export default Footer;
