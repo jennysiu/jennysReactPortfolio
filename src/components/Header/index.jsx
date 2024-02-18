63d9d342e80774b7b4282bb5d0fa4c4149b948e2
jennysiu <jenny.siu79@gmail.com>
2024-02-18 02:07:55 +0000

    added fonts

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
 
+// bootstrap import
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
+// custom css styling
+import "./style.css";
 
 // [x] Must be rendered across the entire site
 // [x] Must Contain a Navbar
@@ -12,15 +16,20 @@ function Header() {
     <>
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand as={Link} to="/">JS Portfolio</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contacts</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <div className="flex-spacer">
+          <Navbar.Brand className="brand-name" as={Link} to="/">Jenny Siu</Navbar.Brand>
+        </div>
+        
+        <div className="navbar-links">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto links-container">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contacts</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
     </>
