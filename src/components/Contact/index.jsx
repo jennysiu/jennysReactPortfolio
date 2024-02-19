e22bf6aa483b44440987c59da059b8fc2a6b9ca4
jennysiu <jenny.siu79@gmail.com>
2024-02-19 00:31:49 +0000

    added contact links

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -7,16 +7,32 @@ import './style.css';
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p className='contact-intro'>Feel free to reach out! I'm always open to discussing new ideas and projects.</p>
+    <div className='contact-container'>
+      <section className='contact-info'>
+        <h1>Get in touch</h1>
+        <p className='contact-intro'>I'd love to hear from you. I'm always open to discussing new ideas and projects.</p>
+        <p>Email: jenny.siu79@gmail.com</p>
+        <div className='contact-links'>
+          <a href="https://github.com/jennysiu">
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/jenny-siu-534576156/">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="mailto:jenny.siu79@gmail.com">
+            <i className="fa-solid fa-envelope"></i>
+          </a>
+        </div>
+      </section>
+
       <Form>
+        <h2>Send me a message</h2>
         <div className='contact-details'>
           <Form.Group className="mb-2" controlId="formBasicEmail">
             <Form.Label></Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
+              I'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
           
@@ -36,15 +52,10 @@ function Contact() {
           <Form.Control as="textarea" rows={3} placeholder="Message"/>
         </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Send
+        <Button className="send-button" variant="primary" type="submit">
+          Send Message
         </Button>
       </Form>
-
-      <section className='contact-info'>
-
-      </section>
-  
     </div>
   )
 }
