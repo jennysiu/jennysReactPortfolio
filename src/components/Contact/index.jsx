186ed0bcd0d26ec7c38979e4ec33feabe861663c
jennysiu <jenny.siu79@gmail.com>
2024-02-19 20:48:23 +0000

    added contact form to enable email notifs

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -25,12 +25,14 @@ function Contact() {
         </div>
       </section>
 
-      <Form>
+      <Form name="contact" data-netlify="true" method="POST" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact-form" />
+
         <h2>Send me a message</h2>
         <div className='contact-details'>
           <Form.Group className="mb-2" controlId="formBasicEmail">
             <Form.Label></Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" name="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
               I'll never share your email with anyone else.
             </Form.Text>
@@ -38,18 +40,18 @@ function Contact() {
           
           <Form.Group className="mb-2" controlId="formBasicPName">
             <Form.Label></Form.Label>
-            <Form.Control type="name" placeholder="Name" />
+            <Form.Control type="name" name="name" placeholder="Name" />
           </Form.Group>
         </div>
 
         <Form.Group className="mb-2" controlId="formBasicSubject">
           <Form.Label></Form.Label>
-          <Form.Control type="subject" placeholder="Subject" />
+          <Form.Control type="subject" name="subject" placeholder="Subject" />
         </Form.Group>
 
         <Form.Group className="mb-2" controlId="exampleForm.ControlTextarea1">
           <Form.Label></Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Message"/>
+          <Form.Control as="textarea" rows={3} name="message"  placeholder="Message"/>
         </Form.Group>
 
         <Button className="send-button" variant="primary" type="submit">
