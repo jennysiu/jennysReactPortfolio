01053716ea3ea88a036dbb28f598bab4142d5c0c
jennysiu <jenny.siu79@gmail.com>
2024-02-19 20:55:38 +0000

    changed form name

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -26,7 +26,7 @@ function Contact() {
       </section>
 
       <Form name="contact" data-netlify="true" method="POST" data-netlify-honeypot="bot-field">
-        <input type="hidden" name="form-name" value="contact-form" />
+        <input type="hidden" name="contact" value="contact-form" />
 
         <h2>Send me a message</h2>
         <div className='contact-details'>
